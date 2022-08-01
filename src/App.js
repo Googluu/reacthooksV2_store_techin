605ced8c757386c71a6fc22416370b188aa6e653
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-08-01 09:26:31 -0500

    useState: estado de componentes creados como funciones :zap:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
